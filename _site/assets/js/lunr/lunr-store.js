a3499b3ab7ad6ef8295aa3620d49308e176748a1
Nicole Zhen <nicolezhen@Nicoles-MacBook-Air.local>
2023-08-08 12:50:13 -0400

    deleated posts section and fixed participation section

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,1 @@
-var store = [{
-        "title": "Call for Participation!",
-        "excerpt":"We are pleased to announce our workshop taking place in conjunction with the 14th International ACM Conference on Automotive User Interfaces and Interactive Vehicular Applications (AutomotiveUI 2022): A3WS - International Perspective on Inclusive External Human-Machine Interfaces In this workshop, we will focus on the accessibility of automated traffic when interacting...","categories": ["updates"],
-        "tags": ["CfP"],
-        "url": "/autoui23.github.io/updates/cfp/",
-        "teaser": null
-      },{
-        "title": "Call for Participation - Extended Deadline!",
-        "excerpt":"Need more time for your postion paper? We decided to extend the deadline for our workshop A3WS - International Perspective on Inclusive External Human-Machine Interfaces In this workshop, we will focus on the accessibility of automated traffic when interacting with vulnerable road user. We look forward to your submissions and...","categories": ["updates"],
-        "tags": ["CfP"],
-        "url": "/autoui23.github.io/updates/cfp2/",
-        "teaser": null
-      }]
+var store = []
